243b8b02ea2c53d3940b7f1b8f42fe4e1b9b739e
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-03-31 07:37:35 +0700

    commit massage

---
+++ Modul5_103022400020/PemrosesData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul5_103022400020
+{
+    public class PemrosesData
+    {
+        public T DapatkanNilaiTerbesar<T>(T t1, T t2, T t3)
+        {
+            dynamic terbesar = t1;
+            dynamic terbesar2 = t2;
+            dynamic terbesar3 = t3;
+
+            if (terbesar2 > terbesar)
+            {
+                terbesar = terbesar2;
+            }
+            if (terbesar3 > terbesar)
+            {
+                terbesar = terbesar3;
+            }
+            return terbesar;
+        }
+    }
+}
--- Modul5_103022400020/Program.cs
+++ Modul5_103022400020/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul5_103022400020;
+public class main
+{
+    static void Main(string[] args)
+    {
+        PemrosesData pd = new PemrosesData();
+
+        long hasil = pd.DapatkanNilaiTerbesar(10, 20, 22);
+        Console.WriteLine("Nilai terbesaer: " + hasil);
+    }
+}
